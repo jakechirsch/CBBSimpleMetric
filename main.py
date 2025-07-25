e2ce864ff342c74104e1077713f4e381135a49af
Jake Hirsch <jakechirsch@gmail.com>
2025-07-25 16:44:24 -0400

    Model v1

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ with open("data_2025.json", "r") as f:
     games = json.load(f)
 """
 
+# Load all teams' data
+with open("data_2025.json", "r") as data_2025:
+    teams = json.load(data_2025)
+with open("name_to_slug.json", "r") as n_to_s:
+    name_to_slug = json.load(n_to_s)
+with open("slug_to_name.json", "r") as s_to_n:
+    slug_to_name = json.load(s_to_n)
+
 def point_diff_calculator():
-    point_diffs = []
-    with open("data_2025.json", "r") as f:
-        teams = json.load(f)
-    with open('teams.txt', 'r') as file:
+    point_diffs = {}
+    with open('teams.csv', 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             games = teams[row['Team']]
@@ -31,20 +37,72 @@ def point_diff_calculator():
                     point_diff += int(game["points_for"]) - int(game["points_against"])
                     reg_games += 1
             point_diff /= reg_games
-            point_diffs.append((row['Team'], point_diff))
-    point_diffs.sort(key=lambda x: x[1], reverse=True)
+            point_diffs[row['Team']] = point_diff
     return point_diffs
 
-def point_diff_score():
-    diffs = point_diff_calculator()
-    worst = diffs[len(diffs) - 1][1]
-    best = diffs[0][1]
+def point_diff_score(diffs):
+    sorted_diffs = rank_teams(diffs)
+    worst = sorted_diffs[len(sorted_diffs) - 1][1]
+    best = sorted_diffs[0][1]
     out_of = best - worst
     scores = []
-    for team_pair in diffs:
+    for team_pair in sorted_diffs:
         scores.append((team_pair[0], 100 * (team_pair[1] - worst) / out_of))
     return scores
 
-ranking = point_diff_score()
-for team in ranking:
-    print(f"{team[0]}: {team[1]}")
+def weighted_point_diff_calculator(scores):
+    sorted_scores = rank_teams(scores)
+    point_diffs = {}
+    for tup in sorted_scores:
+        games = teams[tup[0]]
+        if not games:
+            print(f"No data found for {tup[0]}")
+            sys.exit()
+        point_diff = 0
+        reg_games = 0
+        for game in games:
+            if game["game_type"] == "REG" and game["is_d1_opponent"]:
+                diff = int(game["points_for"]) - int(game["points_against"])
+                opponent = game["opponent"].split("\xa0", 1)[0]
+                game_score = diff + scores[name_to_slug[opponent]]
+                point_diff += game_score
+                reg_games += 1
+        point_diff /= reg_games
+        point_diffs[tup[0]] = point_diff
+    return point_diffs
+
+def rank_teams(diffs):
+    return sorted(diffs.items(), key=lambda item: item[1], reverse=True)
+
+def test_model(model):
+    model_w = 0
+    game_count = 0
+    for team in teams:
+        for t_game in teams[team]:
+            if t_game["is_d1_opponent"]:
+                game_count += 1
+                t_opponent = t_game["opponent"].split("\xa0", 1)[0]
+                if t_game["result"] == "W":
+                    if model[team] > model[name_to_slug[t_opponent]]:
+                        model_w += 1
+                else:
+                    if model[team] < model[name_to_slug[t_opponent]]:
+                        model_w += 1
+    print(f"{int(model_w / 2)}/{int(game_count / 2)}: {model_w / game_count}")
+
+def print_ranking(ranking):
+    rank = 1
+    for team_slug, p_diff in ranking:
+        print(f"{rank}. {slug_to_name[team_slug]}: {p_diff}")
+        rank += 1
+
+p_diffs = point_diff_calculator()
+"""print_ranking(rank_teams(p_diffs))
+test_model(p_diffs)"""
+weighted = []
+for _ in range(10):
+    weighted = weighted_point_diff_calculator(p_diffs)
+    p_diffs = weighted
+weighted_ranking = point_diff_score(weighted)
+print_ranking(weighted_ranking)
+test_model(p_diffs)
